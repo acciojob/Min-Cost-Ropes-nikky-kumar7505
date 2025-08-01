2113749fa852ad8069d12222f6d55db08cceefa9
Nikky Kumar <knikki7505@gmail.com>
2025-08-01 18:08:21 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,18 @@
 function mincost(arr)
 { 
-//write your code here
+//write your code heres
 // return the min cost
-  
+	let total = 0
+	while (arr.length>1) {
+		arr.sort((a,b)=>a-b);
+
+		let first = arr.shift()
+		let second = arr.shift()
+		let cost = first+second
+		total += cost
+		arr.push(cost)
+	}
+	return total;
 }
 
 module.exports=mincost;
